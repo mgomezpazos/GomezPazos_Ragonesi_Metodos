24d964cd420b3055a0c2fc21fbb67ef93511d53f
Fragoesi <fragonesi@udesa.edu.ar>
2023-08-31 16:16:16 -0300

    cambios funcion 2b

--- tp1/Ejercicio2b.py
+++ tp1/Ejercicio2b.py
@@ -2,17 +2,42 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 from math import exp
-from scipy.interpolate import CubicSpline, PchipInterpolator, RectBivariateSpline
+from scipy.interpolate import CubicSpline, PchipInterpolator, RectBivariateSpline, griddata
 
 def functionB(x1, x2):
-    return 0.7 * exp((-(((9*x1)-2)**2)/4) - (((9*x2) - 2)**2)/4) + 0.45*exp((-(((9*x1)+1)**2)/9) - (((9*x2)+1)**2)/5) + 0.55*exp((-(((9*x1)-6)**2)/4)-(((9*x2)-3)**2)/4) - 0.01 * exp((-(((9*x1)-7)**2)/4)-(((9*x2)-3)**2)/4)
+    return (0.7 * np.exp((-(((9*x1)-2)**2)/4) - (((9*x2) - 2)**2)/4)+
+            0.45*np.exp((-(((9*x1)+1)**2)/9) - (((9*x2)+1)**2)/5)+
+            0.55*np.exp((-(((9*x1)-6)**2)/4)-(((9*x2)-3)**2)/4)-
+            0.01 * np.exp((-(((9*x1)-7)**2)/4)-(((9*x2)-3)**2)/4))
 
-#puntos de interpolación de functionB en el intervalo [-1, 1]
+#se crea una malla 3d
 x1 = np.linspace(-1, 1, 10)
 x2 = np.linspace(-1, 1, 10)
 X1, X2 = np.meshgrid(x1, x2)
-interpolation_pointsB = [(x1_value, x2_value) for x1_value in x1 for x2_value in x2]
-real_valuesB = [functionB(x1_value, x2_value) for x1_value, x2_value in interpolation_pointsB]
+Z = functionB(x1, x2)
+#interpolation_pointsB = [(x1_value, x2_value) for x1_value in x1 for x2_value in x2]
+#real_valuesB = [functionB(x1_value, x2_value) for x1_value, x2_value in interpolation_pointsB]
+
+#defino los puntos donde voy a hacer la interpolación 
+x1_interp = np.linspace(-1, 1, 100)
+x2_interp = np.linspace(-1, 1, 100)
+X1_interp, X2_interp = np.meshgrid(x1_interp, x2_interp)
+points = np.column_stack((X1_interp.ravel(), X2_interp.ravel()))
+
+# Realizar la interpolación
+Z_interp = griddata((X1.ravel(), X2.ravel()), Z.ravel(), points, method='cubic')
+Z_interp = Z_interp.reshape(X1_interp.shape)
+
+# Graficar los resultados
+fig = plt.figure(figsize=(12, 6))
+ax1 = fig.add_subplot(121, projection='3d')
+ax1.plot_surface(X1, X2, Z, cmap='viridis')
+ax1.set_title('Función Original')
+
+ax2 = fig.add_subplot(122, projection='3d')
+ax2.plot_surface(X1_interp, X2_interp, Z_interp, cmap='viridis')
+ax2.set_title('Interpolación')
+
 # Realizar interpolación con spline cúbico
 #spline_cubicB = CubicSpline(interpolation_pointsB, real_valuesB)
 # Realizar interpolación con spline quíntico (spline cúbico natural)
