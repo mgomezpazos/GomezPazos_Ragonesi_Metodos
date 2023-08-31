2e6300f76045b344a39326b84eeb13d74186d740
Manuela <mgomezpazos@udesa.edu.ar>
2023-08-31 11:33:54 -0300

    hddsj

--- tp1/Ejercicio2B.py
+++
@@ -1,5 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import math
-from math import exp
-from scipy.interpolate import lagrange, CubicSpline, PchipInterpolator
--- tp1/Ejercicio2b.py
+++ tp1/Ejercicio2b.py
@@ -6,5 +6,3 @@ from scipy.interpolate import lagrange, CubicSpline, PchipInterpolator
 
 def functionB(x1, x2):
     return 0.7 * exp((-(((9*x1)-2)**2)/4) - (((9*x2) - 2)**2)/4) + 0.45*exp((-(((9*x1)+1)**2)/9) - (((9*x2)+1)**2)/5) + 0.55*exp((-(((9*x1)-6)**2)/4)-(((9*x2)-3)**2)/4) - 0.01 * exp((-(((9*x1)-7)**2)/4)-(((9*x2)-3)**2)/4)
-
-inter
