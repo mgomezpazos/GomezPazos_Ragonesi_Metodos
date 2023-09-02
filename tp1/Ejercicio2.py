966a75e7a12c2e91ff54a76b52a3589a39da222a
Fragoesi <fragonesi@udesa.edu.ar>
2023-09-02 15:53:59 -0300

    cambios

--- tp1/Ejercicio2.py
+++ tp1/Ejercicio2.py
@@ -7,6 +7,11 @@ from scipy.interpolate import lagrange, interp1d, CubicSpline, PchipInterpolator
 def functionA(value):
     return 0.05 * abs(value) * np.sin(5 * value) + np.tanh(2 * value) + 2
 
+def generate_chebyshev_nodes(n, a, b):
+    k = np.arange(1, n + 1)
+    chebyshev_nodes = 0.5 * (a + b) + 0.5 * (b - a) * np.cos((2 * k - 1) * np.pi / (2 * n))
+    return chebyshev_nodes
+
 #puntos de interpolación de functionA en el intervalo [-3, 3]
 interpolation_pointsA = np.linspace(-3, 3, 10)
 # Calcular los valores reales de la funciónA en los puntos de interpolación
@@ -36,7 +41,7 @@ plt.plot(evaluation_pointsA, spline_quintic_interpolatedA, label='Spline Quínti
 plt.scatter(interpolation_pointsA, real_valuesA, color='red', label='Puntos de Interpolación')
 plt.xlabel('x')
 plt.ylabel('functionA(x)')
-plt.title('Comparación de Métodos de Interpolación')
+plt.title('Comparación de Métodos de Interpolación con Puntos Equiespaciados')
 plt.legend()
 plt.grid(True)
 plt.show()
@@ -47,7 +52,53 @@ plt.plot(evaluation_pointsA, relative_error_spline_cubic, label='Spline Cúbico'
 plt.plot(evaluation_pointsA, relative_error_spline_quintic, label='Spline Quíntico')
 plt.xlabel('x')
 plt.ylabel('Error Relativo')
-plt.title('Comparación de Error Relativo')
+plt.title('Comparación de Error Relativo con Puntos Equiespaciados')
+plt.legend()
+plt.grid(True)
+plt.show()
+
+# Generate Chebyshev nodes
+interpolation_pointsA = generate_chebyshev_nodes(10, -3, 3)
+interpolation_pointsA = np.sort(interpolation_pointsA)  # Sort the points to ensure they are strictly increasing
+# Calcular los valores reales de la funciónA en los puntos de interpolación
+real_valuesA = functionA(interpolation_pointsA)
+# Realizar interpolación con polinomio de Lagrange
+lagrange_polyA = lagrange(interpolation_pointsA, real_valuesA)
+# Realizar interpolación con spline cúbico
+spline_cubicA = CubicSpline(interpolation_pointsA, real_valuesA)
+# Realizar interpolación con spline quíntico (spline cúbico natural)
+spline_quinticA = PchipInterpolator(interpolation_pointsA, real_valuesA)
+# Puntos donde se evaluarán los resultados
+evaluation_pointsA = np.linspace(-3, 3, 300)  # Más puntos para una representación suave
+# Evaluar los métodos de interpolación en los puntos de evaluación
+lagrange_interpolatedA = lagrange_polyA(evaluation_pointsA)
+spline_cubic_interpolatedA = spline_cubicA(evaluation_pointsA)
+spline_quintic_interpolatedA = spline_quinticA(evaluation_pointsA)
+# Calculate relative errors
+relative_error_lagrange = np.abs(lagrange_interpolatedA - functionA(evaluation_pointsA)) / np.abs(functionA(evaluation_pointsA))
+relative_error_spline_cubic = np.abs(spline_cubic_interpolatedA - functionA(evaluation_pointsA)) / np.abs(functionA(evaluation_pointsA))
+relative_error_spline_quintic = np.abs(spline_quintic_interpolatedA - functionA(evaluation_pointsA)) / np.abs(functionA(evaluation_pointsA))
+# Plotting the comparison of different interpolation methods
+plt.figure(figsize=(10, 6))
+plt.plot(evaluation_pointsA, functionA(evaluation_pointsA), label='Función Original')
+plt.plot(evaluation_pointsA, lagrange_interpolatedA, label='Interpolación Lagrange')
+plt.plot(evaluation_pointsA, spline_cubic_interpolatedA, label='Spline Cúbico')
+plt.plot(evaluation_pointsA, spline_quintic_interpolatedA, label='Spline Quíntico')
+plt.scatter(interpolation_pointsA, real_valuesA, color='red', label='Puntos de Interpolación')
+plt.xlabel('x')
+plt.ylabel('functionA(x)')
+plt.title('Comparación de Métodos de Interpolación con Puntos No Equiespaciados')
+plt.legend()
+plt.grid(True)
+plt.show()
+# Grafico el Error Relativo
+plt.figure(figsize=(10, 6))
+plt.plot(evaluation_pointsA, relative_error_lagrange, label='Lagrange')
+plt.plot(evaluation_pointsA, relative_error_spline_cubic, label='Spline Cúbico')
+plt.plot(evaluation_pointsA, relative_error_spline_quintic, label='Spline Quíntico')
+plt.xlabel('x')
+plt.ylabel('Error Relativo')
+plt.title('Comparación de Error Relativo con Puntos No Equiespaciados')
 plt.legend()
 plt.grid(True)
 plt.show()
--- tp1/Ejercicio2b.py
+++ tp1/Ejercicio2b.py
@@ -10,7 +10,7 @@ def functionB(x1, x2):
             0.55*np.exp((-(((9*x1)-6)**2)/4)-(((9*x2)-3)**2)/4)-
             0.01 * np.exp((-(((9*x1)-7)**2)/4)-(((9*x2)-3)**2)/4))
 
-
+#puntos equiespaciados
 x1 = np.linspace(-1, 1, 10)
 x2 = np.linspace(-1, 1, 10)
 X1, X2 = np.meshgrid(x1, x2)
@@ -26,59 +26,83 @@ points = np.column_stack((X1_interp.ravel(), X2_interp.ravel()))
 Z_interp_equi = griddata((X1.ravel(), X2.ravel()), Z.ravel(), points, method='cubic')
 Z_interp_equi = Z_interp_equi.reshape(X1_interp.shape)
 
-
-
-
 #graficos
 fig, axes = plt.subplots(1, 2, figsize=(18, 6), subplot_kw={'projection': '3d'})
 
-axes[0].plot_surface(X1, X2, Z, cmap='viridis')
+surface1 = axes[0].plot_surface(X1, X2, Z, cmap='viridis')
 axes[0].set_title('Función Original')
 
-axes[1].plot_surface(X1_interp, X2_interp, Z_interp_equi, cmap='viridis')
+#barra de variación
+cbar1 = plt.colorbar(surface1, ax=axes[0], shrink=0.5, aspect=10)
+cbar1.set_label('Variación de Z')
+
+surface2 = axes[1].plot_surface(X1_interp, X2_interp, Z_interp_equi, cmap='viridis')
 axes[1].set_title('Interpolación con puntos equiespaciados')
+
+#barra de variación
+cbar2 = plt.colorbar(surface2, ax=axes[1], shrink=0.5, aspect=10)
+cbar2.set_label('Variación de Z')
 
 plt.show()
 
-#if _name_ == "_main_":
- #   main()
-
-# #se crea una malla 3d
-# x1 = np.linspace(-1, 1, 10)
-# x2 = np.linspace(-1, 1, 10)
-# X1, X2 = np.meshgrid(x1, x2)
-# Z = functionB(x1, x2)
-# #interpolation_pointsB = [(x1_value, x2_value) for x1_value in x1 for x2_value in x2]
-# #real_valuesB = [functionB(x1_value, x2_value) for x1_value, x2_value in interpolation_pointsB]
-
-# #defino los puntos donde voy a hacer la interpolación 
-# x1_interp = np.linspace(-1, 1, 100)
-# x2_interp = np.linspace(-1, 1, 100)
-# X1_interp, X2_interp = np.meshgrid(x1_interp, x2_interp)
-# points = np.column_stack((X1_interp.ravel(), X2_interp.ravel()))
-
-# # Realizar la interpolación
-# Z_interp = griddata((X1.ravel(), X2.ravel()), Z.ravel(), points, method='cubic')
-# Z_interp = Z_interp.reshape(X1_interp.shape)
-
-# # Graficar los resultados
-# fig = plt.figure(figsize=(12, 6))
-# ax1 = fig.add_subplot(121, projection='3d')
-# ax1.plot_surface(X1, X2, Z, cmap='viridis')
-# ax1.set_title('Función Original')
-
-# ax2 = fig.add_subplot(122, projection='3d')
-# ax2.plot_surface(X1_interp, X2_interp, Z_interp, cmap='viridis')
-# ax2.set_title('Interpolación')
-
-# # Realizar interpolación con spline cúbico
-# #spline_cubicB = CubicSpline(interpolation_pointsB, real_valuesB)
-# # Realizar interpolación con spline quíntico (spline cúbico natural)
-# #spline_quinticB = PchipInterpolator(interpolation_pointsB, real_valuesB)
-# # Create 2D interpolator
-# interp_B = RectBivariateSpline(x1, x2, real_valuesB, kx=3, ky=3)
-# # Puntos donde se evaluarán los resultados
-# evaluation_pointsB = np.linspace(-1, 1, 300) 
+#calculamos el error relativo
+# evaluation_pointsA = np.linspace(-3, 3, 300) 
 # # Evaluar los métodos de interpolación en los puntos de evaluación
-# #spline_cubic_interpolatedB = spline_cubicB(evaluation_pointsB)
-# #spline_quintic_interpolatedB = spline_quinticB(evaluation_pointsB)
+# lagrange_interpolatedA = lagrange_polyA(evaluation_pointsA)
+# spline_cubic_interpolatedA = spline_cubicA(evaluation_pointsA)
+# spline_quintic_interpolatedA = spline_quinticA(evaluation_pointsA)
+# # Calculate relative errors
+# relative_error_lagrange = np.abs(lagrange_interpolatedA - functionA(evaluation_pointsA)) / np.abs(functionA(evaluation_pointsA))
+# relative_error_spline_cubic = np.abs(spline_cubic_interpolatedA - functionA(evaluation_pointsA)) / np.abs(functionA(evaluation_pointsA))
+# relative_error_spline_quintic = np.abs(spline_quintic_interpolatedA - functionA(evaluation_pointsA)) / np.abs(functionA(evaluation_pointsA))
+
+error_relativo = np.abs(Z - Z_interp_equi) / np.abs(Z)
+average_error = np.mean(error_relativo)
+
+# Mostrar el error relativo promedio
+print("Error Relativo Promedio:", average_error)
+
+# Gráfico del error relativo
+plt.imshow(error_relativo, cmap='viridis', extent=(-1, 1, -1, 1), origin='lower')
+plt.colorbar(label='Error Relativo')
+plt.title('Error Relativo entre la Función Original y la Interpolación')
+plt.xlabel('x1')
+plt.ylabel('x2')
+plt.show()
+
+# Puntos de Chebyshev no equiespaciados
+x1_cheb = chebpts2(10)
+x2_cheb = chebpts2(10)
+X1_cheb, X2_cheb = np.meshgrid(x1_cheb, x2_cheb)
+Z_cheb = functionB(X1_cheb, X2_cheb)
+
+# Puntos para interpolar (también en Chebyshev)
+x1_interp_cheb = chebpts2(100)
+x2_interp_cheb = chebpts2(100)
+X1_interp_cheb, X2_interp_cheb = np.meshgrid(x1_interp_cheb, x2_interp_cheb)
+points_cheb = np.column_stack((X1_interp_cheb.ravel(), X2_interp_cheb.ravel()))
+
+# Interpolación cúbica con griddata
+Z_interp_cheb = griddata((X1_cheb.ravel(), X2_cheb.ravel()), Z_cheb.ravel(), points_cheb, method='cubic')
+Z_interp_cheb = Z_interp_cheb.reshape(X1_interp_cheb.shape)
+
+# Gráficos
+fig, axes = plt.subplots(1, 2, figsize=(18, 6), subplot_kw={'projection': '3d'})
+
+# Gráfico 1: Función Original con puntos Chebyshev
+surface1 = axes[0].plot_surface(X1_cheb, X2_cheb, Z_cheb, cmap='viridis')
+axes[0].set_title('Función Original (Chebyshev)')
+
+# Barra de variación
+cbar1 = plt.colorbar(surface1, ax=axes[0], shrink=0.5, aspect=10)
+cbar1.set_label('Variación de Z')
+
+# Gráfico 2: Interpolación con puntos Chebyshev
+surface2 = axes[1].plot_surface(X1_interp_cheb, X2_interp_cheb, Z_interp_cheb, cmap='viridis')
+axes[1].set_title('Interpolación con puntos Chebyshev')
+
+# Barra de variación
+cbar2 = plt.colorbar(surface2, ax=axes[1], shrink=0.5, aspect=10)
+cbar2.set_label('Variación de Z')
+
+plt.show()
