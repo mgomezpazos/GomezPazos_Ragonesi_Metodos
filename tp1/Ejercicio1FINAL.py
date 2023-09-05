35fb7a5f19075c7ac2d021a5208a2e36793fb967
Manuela <mgomezpazos@udesa.edu.ar>
2023-09-04 22:09:29 -0300

    updates

--- tp1/Ejercicio1FINAL.py
+++ tp1/Ejercicio1FINAL.py
@@ -97,40 +97,49 @@ plt.legend()
 plt.grid(True)
 plt.show()
 #------------------------------------------------------------------BÚSQUEDA DE RAÍCES---------------------------------------------------------------------------------------------
-# Define la función que representa la línea recta
-def line_equation(x, y):
-    return 0.35 * x + y - 3.6
+def f1(x1, x2):
+    return x1 - 10  # Ecuación de la recta x1 = 10
 
-# Función para encontrar un punto de cruce utilizando el método de Newton-Raphson
-def find_intersection(interpolation_function, line_equation, x_guess, y_guess):
-    # Función que representa la diferencia entre la función interpolada y la línea recta
-    def equation_to_solve(point):
-        x, y = point
-        return interpolation_function(x) - line_equation(x, y)
+def f2(x1, x2):
+    return 0.35 * x1 + x2 - 3.6  # Ecuación de la recta 0.35 * x1 + x2 = 3.6
 
-    # Encuentra las coordenadas (x, y) donde la función cruza la línea recta
-    intersection_point = newton(equation_to_solve, (x_guess, y_guess))
+# Inicializa las estimaciones iniciales para Newton-Raphson
+x1_initial_guess = 0.0
+x2_initial_guess = 0.0
 
-    return intersection_point
+# Función para aplicar el método de Newton-Raphson
+def newton_raphson_2d(f1, f2, x1_guess, x2_guess, tol=1e-6, max_iter=100):
+    x1 = x1_guess
+    x2 = x2_guess
+    for i in range(max_iter):
+        f1_val = f1(x1, x2)
+        f2_val = f2(x1, x2)
+        if abs(f1_val) < tol and abs(f2_val) < tol:
+            return x1, x2
+        # Calcula las derivadas parciales
+        df1_dx1 = newton(lambda x: f1(x, x2), x1)
+        df1_dx2 = newton(lambda x: f1(x1, x), x2)
+        df2_dx1 = newton(lambda x: f2(x, x2), x1)
+        df2_dx2 = newton(lambda x: f2(x1, x), x2)
+        # Actualiza las estimaciones
+        x1 -= (f1_val * df2_dx2 - f2_val * df1_dx2) / (df1_dx1 * df2_dx2 - df1_dx2 * df2_dx1)
+        x2 -= (f2_val * df1_dx1 - f1_val * df2_dx1) / (df1_dx1 * df2_dx2 - df1_dx2 * df2_dx1)
+    raise Exception("El método de Newton-Raphson no convergió después de {} iteraciones.".format(max_iter))
 
-# Supongamos que deseas encontrar el primer punto de cruce en el punto inicial (x, y)
-x_initial_guess = 5.0
-y_initial_guess = 2.0
+# Aplica el método de Newton-Raphson para encontrar las intersecciones
+x1_intersection, x2_intersection = newton_raphson_2d(f1, f2, x1_initial_guess, x2_initial_guess)
 
-# Encuentra el primer punto de cruce
-first_intersection_point = find_intersection(x_t, line_equation, x_initial_guess, y_initial_guess)
-
-print("Primer punto de cruce:", first_intersection_point)
-
-# Modifica la ecuación para excluir el primer punto de cruce
-def modified_line_equation(x, y):
-    return 0.35 * x + y - 3.6 if (x, y) != first_intersection_point else 0
-
-# Supongamos que deseas encontrar el segundo punto de cruce en el punto inicial (x, y)
-x_initial_guess = 10.0
-y_initial_guess = 4.0
-
-# Encuentra el segundo punto de cruce
-second_intersection_point = find_intersection(x_t, modified_line_equation, x_initial_guess, y_initial_guess)
-
-print("Segundo punto de cruce:", second_intersection_point)
+# Plotea las intersecciones en el segundo gráfico
+plt.figure(figsize=(10, 6))
+plt.scatter(x1_ti, x2_ti, label='Data Points', color='purple')
+plt.plot(x_ground_truth, y_ground_truth, label='Ground Truth Function', linestyle='--', color='green')
+plt.plot([10] * len(x1_interp), x2_interp, label='x1 = 10', linestyle='-.', color='red', linewidth=2)
+plt.plot(x1_custom[indices_within_limits], x2_custom[indices_within_limits], label='0.35*x1 + x2 = 3.6', linestyle='-.', color='orange')
+plt.plot(x1_interp, x2_interp, label='Cubic Spline Interpolation', color='blue')
+plt.plot(x1_intersection, x2_intersection, 'ro', label='Intersections', markersize=8)  # Marca las intersecciones en rojo
+plt.xlabel('X1(t)')
+plt.ylabel('X2(t)')
+plt.title('Comparison of Interpolated Spline and Ground Truth Functions with Intersections')
+plt.legend()
+plt.grid(True)
+plt.show()
